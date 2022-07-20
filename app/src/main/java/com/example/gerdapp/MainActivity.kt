3d12b6b67596b5da43a28118936a2079b80aad05
peggy <peggy1210.huang@gmail.com>
2022-07-20 14:55:36 +0800

    update strings for app navigation

--- app/src/main/java/com/example/gerdapp/MainActivity.kt
+++ app/src/main/java/com/example/gerdapp/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.gerdapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.gerdapp.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,30 +25,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val bottomNavView: BottomNavigationView = binding.navView
-
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_main, R.id.navigation_questionnaire, R.id.navigation_chart))
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_main,
+                R.id.navigation_calendar,
+                R.id.navigation_chart,
+                R.id.navigation_profile
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.apply {
-            bottomNavView.setOnItemSelectedListener { item ->
-                when(item.itemId) {
-                    R.id.list -> {
+            navView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.bottom_nav_home -> {
                         navController.navigate(R.id.navigation_main)
                         true
                     }
-                    R.id.questionnaire -> {
+                    R.id.bottom_nav_list -> {
                         navController.navigate(R.id.navigation_calendar)
                         true
                     }
-                    R.id.statistics -> {
+                    R.id.bottom_nav_stat -> {
                         navController.navigate(R.id.navigation_chart)
                         true
                     }
-                    R.id.setting -> {
+                    R.id.bottom_nav_prof -> {
                         navController.navigate(R.id.navigation_profile)
                         true
                     }
@@ -58,18 +61,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-//        navView = binding.navView
-//
-//        navController = findNavController(R.id.nav_host_fragment_activity_main)
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_food, R.id.navigation_main, R.id.navigation_food, R.id.navigation_food
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
